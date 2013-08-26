0faa73210298bd52612aa3c4893732de89406b42
J C Lawrence <claw@kanga.nu>
2013-08-26 00:10:40 -0700

    now() and time_str()

--- logtool/__init__.py
+++ logtool/__init__.py
@@ -4,3 +4,4 @@ import pyver
 __version__, __version_info__ = pyver.get_version (pkg = __name__)
 from logtool.log_wrap import log_func
 from logtool.log_fault_impl import log_fault
+from logtool.logtime import now, time_str
---
+++ logtool/logtime.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+import logging
+from datetime import datetime
+from time import gmtime, strftime, time
+from logtool import log_func
+
+DEFAULT_TIMEFORMAT = "%H:%M:%S %a %d %b %Y Z+0000"
+
+LOG = logging.getLogger (__name__)
+
+@log_func
+def time_str (time_t):
+  '''Converts floating point number a'la time.time()
+  using DEFAULT_TIMEFORMAT
+  '''
+  return datetime.fromtimestamp (int (time_t)).strftime (DEFAULT_TIMEFORMAT)
+
+@log_func
+def now (time_t = None):
+  '''Gives current time as tuple (t, t_str) where
+     t is integer portion from time.time()
+     t_str string of t using DEFAULT_TIMEFORMAT
+  '''
+  if not time_t:
+    time_t = time ()
+  time_s = strftime (DEFAULT_TIMEFORMAT, gmtime (time_t))
+  return (int (time_t), time_s)
