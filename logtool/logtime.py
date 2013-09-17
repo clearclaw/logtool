a01e5363c2014381cccc2a82a99c73685d75d7e2
J C Lawrence <claw@kanga.nu>
2013-09-16 17:47:06 -0700

    Solid time for now()

--- logtool/logtime.py
+++ logtool/logtime.py
@@ -6,6 +6,7 @@ from time import gmtime, strftime, time
 from logtool import log_func
 
 DEFAULT_TIMEFORMAT = "%H:%M:%S %a %d %b %Y Z+0000"
+DEFAULT_SOLIDFORMAT = "%Y-%m-%d-%H%M.%S-Z0000"
 
 LOG = logging.getLogger (__name__)
 
@@ -17,12 +18,15 @@ def time_str (time_t):
   return datetime.fromtimestamp (int (time_t)).strftime (DEFAULT_TIMEFORMAT)
 
 @log_func
-def now (time_t = None):
+def now (time_t = None, solid = False):
   '''Gives current time as tuple (t, t_str) where
      t is integer portion from time.time()
      t_str string of t using DEFAULT_TIMEFORMAT
   '''
   if not time_t:
     time_t = time ()
-  time_s = strftime (DEFAULT_TIMEFORMAT, gmtime (time_t))
+  if not solid:
+    time_s = strftime (DEFAULT_TIMEFORMAT, gmtime (time_t))
+  else:
+    time_s = strftime (DEFAULT_SOLIDFORMAT, gmtime (time_t))
   return (int (time_t), time_s)
