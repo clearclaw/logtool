35bdf48ff758df9982e2a064b701f0bce8d8bb77
J C Lawrence <jcl@matterport.com>
2014-09-18 15:04:04 -0700

    log_func_noargs

--- logtool/__init__.py
+++ logtool/__init__.py
@@ -2,6 +2,6 @@
 
 import pyver
 __version__, __version_info__ = pyver.get_version (pkg = __name__)
-from logtool.log_wrap import log_func
+from logtool.log_wrap import log_func, log_func_noargs
 from logtool.log_fault_impl import log_fault
 from logtool.logtime import now, time_str
