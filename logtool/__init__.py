cdf3bb2e16a73f66bfacafd4414e6ffa4eb148ce
J C Lawrence <jcl@matterport.com>
2015-07-07 17:37:01 -0700

    log_fault_einfo,

--- logtool/__init__.py
+++ logtool/__init__.py
@@ -4,5 +4,5 @@ import pyver
 __version__, __version_info__ = pyver.get_version (pkg = __name__)
 from logtool.log_wrap import log_func, log_func_noargs, log_call, log_trace
 from logtool.log_fault_impl import log_fault, log_fault_exc_str
-from logtool.log_fault_impl import log_fault_info_str
+from logtool.log_fault_impl import log_fault_einfo, log_fault_info_str
 from logtool.logtime import now, time_str
