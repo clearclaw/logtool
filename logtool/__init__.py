4973eafa5c463f79bd045221acdf290c23c7c34b
J C Lawrence <claw@kanga.nu>
2015-08-09 21:35:17 -0700

    versioneer

--- logtool/__init__.py
+++ logtool/__init__.py
@@ -1,8 +1,18 @@
 #! /usr/bin/env python
 
-import pyver
-__version__, __version_info__ = pyver.get_version (pkg = __name__)
-from logtool.log_wrap import log_func, log_func_noargs, log_call, log_trace
-from logtool.log_fault_impl import log_fault, log_fault_exc_str
-from logtool.log_fault_impl import log_fault_einfo, log_fault_info_str
-from logtool.logtime import now, time_str
+from __future__ import absolute_import
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+__version_info__ = get_versions ()
+del get_versions
+
+from .log_wrap import log_func, log_func_noargs, log_call, log_trace
+from .log_fault_impl import log_fault, log_fault_exc_str
+from .log_fault_impl import log_fault_einfo, log_fault_info_str
+from .logtime import now, time_str
+
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -1,36 +1,27 @@
 #! /usr/bin/env python
 
-try:
-  import pyver
-except ImportError:
-  import pip
-  pip.main (['install', 'pyver'])
-  import pyver # pylint: disable=W0611
-
 from setuptools import setup, find_packages
-
-__version__, __version_info__ = pyver.get_version (pkg = "logtool",
-                                                   public = True)
+import versioneer
 
 setup (
     name = "logtool",
-    version = __version__,
+    version = versioneer.get_version (),
     description = "Methods and tools that assist logging.",
     long_description = file ("README.rst").read (),
+    cmdclass = versioneer.get_cmdclass (),
     classifiers = [
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: "
-        + "GNU General Public License v3 or later (GPLv3+)",
+        + "GNU Lesser General Public License v3 or later (LGPLv3+)",
         "Topic :: Utilities",
     ],
     keywords = "logging, exceptions, callpoints, call tracing",
     author = "J C Lawrence",
     author_email = "claw@kanga.nu",
     url = "https://github.com/clearclaw/logtool",
-    download_url = ("https://github.com/clearclaw/logtool/tarball/%s.%s"
-                    % (__version_info__[0], __version_info__[1])),
     license = "LGPL v3.0",
+    test_suite = "tests",
     packages = find_packages (exclude = ["tests",]),
     package_data = {},
     data_files = [],
